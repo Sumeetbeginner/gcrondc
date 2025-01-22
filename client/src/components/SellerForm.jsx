4a3ba078a5e6df68508e63759bfecf62dd84b072
Sumeet Gupta <sumeetgupta3690@gmail.com>
2025-01-22 23:11:09 +0530

    Update SellerForm.jsx

--- client/src/components/SellerForm.jsx
+++ client/src/components/SellerForm.jsx
@@ -1,23 +1,27 @@
 import React, { useState } from 'react'
 import SingleUpload from './SingleUpload'
 import BulkUpload from './BulkUpload'
+import AddTemplate from './seller/AddTemplate'
 
-export default function SellerForm({activeSingle, setActiveSingle, sid}) {
+export default function SellerForm({ activeSingle, setActiveSingle, sid }) {
 
+  const [regular, setRegular] = useState(true)
   // let sid = localStorage.getItem('sid') || "677bdf07dd9457b5d727c6cf";
 
-  
+
   return (
     <div className='sellerForm'>
       <div className="s-tabs">
-        <li className={!activeSingle?'s-t-active':''} onClick={()=>setActiveSingle(true)}> <i className='fas fa-upload'></i> Single Product Upload</li>
-        <li className={activeSingle?'s-t-active':''} onClick={()=>{setActiveSingle(false),console.log("activeSingle is setted to false");
+        <li className={!activeSingle ? 's-t-active' : ''} onClick={() => {setActiveSingle(true), setRegular(true)}}> <i className='fas fa-upload'></i> Single Product Upload</li>
+        <li className={activeSingle ? 's-t-active' : ''} onClick={() => {
+          setActiveSingle(false), console.log("activeSingle is setted to false");
         }}> <i className='fas fa-file'></i> Multiple Product Upload</li>
       </div>
       {
-        activeSingle 
-        ? (<SingleUpload sid={sid}/>)
-        :(<BulkUpload sid={sid}/>)
+        activeSingle
+          ? (
+            regular ? (<SingleUpload sid={sid} setRegular={setRegular} />) : <AddTemplate sid={sid} setRegular={setRegular}/>
+          ) : (<BulkUpload sid={sid} />)
       }
     </div>
   )
